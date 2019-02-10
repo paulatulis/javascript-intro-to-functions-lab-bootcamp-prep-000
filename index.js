7448d02fd7b06f6c1ba3e5586f343e8abf703f53
Paula Tulis <tulis.paula@gmail.com>
2019-02-10 22:18:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,8 @@ function sayHiToGrandma(string){
   if (string===string.toLowerCase()) return "I can't hear you!";
   else if (string === string.toUpperCase()) return "YES INDEED!";
 }
+function sayHiToGrandma(string){
+  if (string===string.toLowerCase()) return "I can't hear you!";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+  else if (string === "I love you, Grandma.") return "I love you, too."
+}
