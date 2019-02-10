403657da0347950768329a2668ca4e6a1444ac45
Paula Tulis <tulis.paula@gmail.com>
2019-02-10 21:53:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function whisper(string) {
 function logShout(string){
   console.log(string.toUpperCase())
 }
-whisper("HELLO")
+logWhisper("HELLO")
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
