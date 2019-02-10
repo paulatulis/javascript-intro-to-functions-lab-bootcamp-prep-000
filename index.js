7b7bae677f70a83d917fb02494daa48b9cfef1d3
Paula Tulis <tulis.paula@gmail.com>
2019-02-10 22:14:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function sayHiToGrandma(string){
 }
 function sayHiToGrandma(string){
   if (string===string.toLowerCase()) return "I can't hear you!";
-  else (string ===string.toUpperCase()) return "YES INDEED!";
+  else if (string ===string.toUpperCase()) return "YES INDEED!";
 }
