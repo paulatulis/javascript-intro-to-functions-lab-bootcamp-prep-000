73ed8db91ef1b375c098303630d54e5057ac04fc
Paula Tulis <tulis.paula@gmail.com>
2019-02-10 22:02:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,4 @@ function logWhisper(string){
 }
 function sayHiToGrandma (string){
   if(string===string.toLowerCase()) return ("I can't hear you!")
-  if(string===string.toUpperCase()) return ("YES INDEED")
-  
 }
