4d7e8ddbadde1052e9ed7b8f63ddbbdf986a760f
Paula Tulis <tulis.paula@gmail.com>
2019-02-10 20:28:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase ()
 }
-function logShout(string){
-  log.console (string.toUpperCase)
+function expect (string){
+  expect (string.toUpperCase)
 }
